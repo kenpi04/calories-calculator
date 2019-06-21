905e3be404fafb61002920a79cd7273ec196f292
Tan quang <kenpi04@gmail.com>
2019-06-21 11:30:22 +0700

    'a'

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import express from "express";
+const express =require("express");
 const app=express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 6969;
 
 app.get('/',(req,res)=>{
-    res.write("Server run");
+    console.log("home")
+    res.send("Server run");
 });
 
 app.listen(PORT,()=>console.log("servicer start at"+PORT));
