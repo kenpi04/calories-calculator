3460b563572b86f5e5fef4bdea36837e5a121995
Tan quang <kenpi04@gmail.com>
2019-06-21 16:42:54 +0700

    'a'

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+const CloriesService =require("./services/CloriesService.js");
 const express =require("express");
 const app=express();
 const PORT = process.env.PORT || 6969;
 
-app.get('/',(req,res)=>{
-    console.log("home")
-    res.send("Server run");
+app.get('/',async(req,res)=>{
+
+    const keyword=req.query.keyword;
+    const pageIndex=req.query.pageindex;
+    const pageSize=req.query.pagesize;
+    console.log(req.query);
+    
+    var data=await CloriesService.searchData(keyword,pageIndex,pageSize);
+    res.send(data);
+    
+    
+ 
 });
 
 app.listen(PORT,()=>console.log("servicer start at"+PORT));
